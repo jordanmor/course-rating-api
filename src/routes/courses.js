1bca2f497b01512c9669a87bb1d635ab946af3a4
Jordan Mor <jordanmor@gmail.com>
2018-10-04 14:47:23 -0400

    Add course routes

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,12 +5,25 @@ const { Review } = require('../models/review');
 const { User } = require('../models/user');
 
 
-router.get('/', (req, res) => {
-    Course.find().select('title').then(courses => res.send(courses));
+router.get('/', async (req, res) => {
+  const courses = await Course.find().select('title');
+  res.send(courses);
 });
 
-router.get('/:courseId', (req, res) => {
-  Course.findById(req.params.courseId)
+router.post('/', (req, res, next) => {
+  const course = new Course(req.body);
+  course.save((err, course) => {
+    if (err) {
+      err.status = 400;
+      return next(err);
+    }
+    res.location('/').status(201).end();
+  });
+});
+
+router.get('/:courseId', (req, res, next) => {
+  Course
+    .findById(req.params.courseId)
     .populate({
       path: 'reviews',
       // Populating across multiple levels
@@ -20,7 +33,50 @@ router.get('/:courseId', (req, res) => {
       }
     })
     .populate('user', '_id fullName') // deep population
-    .then(data => res.send(data));
+    .then(course => res.send(course))
+    .catch(err => {
+      err.message = 'The course with the given ID was not found.';
+      err.status = 404;
+      next(err);
+    });
+});
+
+router.put('/:courseId', (req, res, next) => {
+  Course.findOneAndUpdate(
+    { _id: req.params.courseId }, 
+    req.body, 
+    (err, course) => {
+      if(err) {
+        err.status = 400;
+        return next(err);
+      }
+      res.status(204).end();
+    });
+});
+
+router.post('/:courseId/reviews', async (req, res, next) => {
+
+  const course = await Course
+    .findById(req.params.courseId)
+    .populate('reviews')
+    .populate('user', '_id fullName');
+
+  const review = new Review(req.body);
+  course.reviews.push(review);
+
+  course.save((err, course) => {
+    if (err) {
+      err.status = 400;
+      return next(err);
+    }
+    review.save((err, course) => {
+      if (err) {
+        err.status = 400;
+        return next(err);
+      }
+    });
+    res.location(`/api/courses/${req.params.courseId}`).status(201).end();
+  });
 });
 
 module.exports = router;
