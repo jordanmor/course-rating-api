40104271a48345168f5a8cdb54903eb82fe8fc8c
Jordan Mor <jordanmor@gmail.com>
2018-10-03 20:18:02 -0400

    Return Course properties & related documents using deep population

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const models = require('../models');
-const { Course } = models;
+const { Course } = require('../models/course');
+const { Review } = require('../models/review');
+const { User } = require('../models/user');
+
 
 router.get('/', (req, res) => {
-    res.send('Courses');
+    Course.find().select('title').then(courses => res.send(courses));
+});
+
+router.get('/:courseId', (req, res) => {
+  Course.findById(req.params.courseId)
+    .populate({
+      path: 'reviews',
+      // Populating across multiple levels
+      populate: {
+        path: 'user',
+        select: '_id fullName' // deep population
+      }
+    })
+    .populate('user', '_id fullName') // deep population
+    .then(data => res.send(data));
 });
 
 module.exports = router;
