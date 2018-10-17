3e3a2a4622f2864d8bf0f479154ae0676ddc32c7
Jordan Mor <jordanmor@gmail.com>
2018-10-17 07:44:27 -0400

    Create an authentication method on the user model

--- src/models/user.js
+++ src/models/user.js
@@ -24,6 +24,27 @@ const UserSchema = new Schema({
   }
 });
 
+// User authentication
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ emailAddress: email })
+      .exec(function(error, user) {
+        if(error) {
+          return callback(error);
+        } else if (!user) {
+          const err = new Error('User not found');
+          err.status = 401;
+          return callback(err);
+        }
+        bcrypt.compare(password, user.password, function(error, result) {
+          if (result === true) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        })
+      });
+}
+
 // Hash password before saving to database
 UserSchema.pre('save', function(next) {
   const user = this;
