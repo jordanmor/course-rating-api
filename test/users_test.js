d4dec980657e4fd781ded39d936104631f005b76
Jordan Mor <jordanmor@gmail.com>
2018-10-19 13:04:19 -0400

    Unit test GET /api/users invalid creds returns 401 status

--- test/users_test.js
+++ test/users_test.js
@@ -2,7 +2,9 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 
+const app = require('../src/index');
 const { expect } = chai;
+
 chai.use(chaiHttp);
 
 // Test suite
@@ -15,8 +17,13 @@ describe('GET /api/users', function () {
   });
 
   it('should return a 401 status error when using invalid credentials', done => {
-    expect(true).to.be.ok;
-    done();
+    chai.request(app)
+      .get('/api/users')
+      // No Basic authentication used
+      .end( (err, res) => {
+        expect(res).to.have.status(401);
+        done();
+    });
   });
 
 });
