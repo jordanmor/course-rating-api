1411d7e057e02f17fd81b9a008e7691f6a88152c
Jordan Mor <jordanmor@gmail.com>
2018-10-19 15:18:04 -0400

    Unit test GET /api/users valid creds returns user doc

--- test/users_test.js
+++ test/users_test.js
@@ -3,17 +3,38 @@ const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 
 const app = require('../src/index');
-const { expect } = chai;
+const { User } = require('../src/models/user');
 
+const { expect } = chai;
 chai.use(chaiHttp);
 
 // Test suite
 describe('GET /api/users', function () {
 
+  // Data used to create a new user in the database and test if that user is found with a GET request
+  const fullName = 'Harry Potter';
+  const emailAddress = 'harrypotter@hogwarts.com';
+  const password = 'magic';
 
   it('should return the corresponding user document when using correct credentials', done => {
-    expect(true).to.be.ok;
-    done();
+
+    // create a user to save into the database and test
+    const user = new User({ fullName, emailAddress, password});
+
+    user.save(function(err, user) {
+      chai.request(app)
+        .get('/api/users')
+        // Authenticate with Basic authentication
+        .auth(emailAddress, password)
+        .end( function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          /* Since each email address in the database is unique, 
+          a match proves the same user designated above was returned in the response */
+          expect(res.body).to.have.property('emailAddress').eql(emailAddress);
+          done();
+        });
+    });
   });
 
   it('should return a 401 status error when using invalid credentials', done => {
@@ -23,7 +44,14 @@ describe('GET /api/users', function () {
       .end( (err, res) => {
         expect(res).to.have.status(401);
         done();
-    });
+      });
   });
+
+  after(function() {
+    // Delete user from database after unit tests complete
+    User.findOneAndDelete({emailAddress}, function(err, user) {
+      if(err) {throw err };
+    });
+  })
 
 });
